46e304cad5c5c1951bffe620ccff3037ab5f5caf
Will23062009 <71350630+Will23062009@users.noreply.github.com>
2023-10-29 15:54:41 -0400

    Update and add csv files
    Updated the code so that it can read the csv files with a small user menu.

--- python.py
+++ python.py
@@ -1,5 +1,54 @@
 import os
 import sys
+import csv
+
+def string_to_int(str) -> int:
+    try:
+        output = int(str)
+    except ValueError:
+        print("invalid Input. Enter Integer value\n")
+        return 5
+    return output
+
+def read_csv(file_path):
+    try:
+        with open(file_path, 'r', newline='') as csvfile:
+            reader = csv.reader(csvfile)
+            for row in reader:
+                print(row)
+    except FileNotFoundError:
+        print(f"File not found: {file_path}")
+    except Exception as e:
+        print(f"An error occurred: {str(e)}")
+
+def main():
+    script_directory = os.path.dirname(os.path.abspath(__file__))  # Get the directory of the script
+    abalone_file_path = os.path.join(script_directory, "abalone.csv")
+    penguins_file_path = os.path.join(script_directory, "penguins.csv")
+    while(True):
+        user_input = input("Please select one of the options below to check a file.\n(1) abalone.csv\n(2) penguins.csv\n(3) custome file (File path is required)\n")
+        user_choice = string_to_int(user_input)
+        if user_input and user_choice <= 3:
+            if user_choice == 1:
+                print("abalone.csv has been selected")
+                read_csv(abalone_file_path)
+                break
+            elif user_choice == 2:
+                print("penguins.csv has been selected")
+                read_csv(penguins_file_path)
+                break
+            elif user_choice == 3:
+                while(True):
+                    user_input_custom = input("Custom file has been selects\nPlease enter the file path and name that you want to review")
+                    if user_input_custom: 
+                        read_csv(user_input)
+                        break
+                    else:
+                        print("[Error]: User input is empty. Please enter a valid file.")    
+            else:
+                print("[Error]: Invalide option has been selected.")
+        else:
+            print("[Error]: User input is empty. Please enter a valid file.")
 
 if __name__ == "__main__":
-    print("Hello World!")
+    main()
