2bb28d69afc0b5f28b7564a562d6c49f28599eda
Will23062009 <71350630+Will23062009@users.noreply.github.com>
2023-11-05 17:17:51 -0500

    4B added
    Added 4.B and some other minor updates to the code.

--- python.py
+++ python.py
@@ -6,10 +6,11 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt #https://matplotlib.org/3.5.3/api/_as_gen/matplotlib.pyplot.html    
 from sklearn.model_selection import train_test_split #https://scikit-learn.org/stable/modules/generated/sklearn.model_selection.train_test_split.html#sklearn.model_selection.train_test_split
-from sklearn.tree import DecisionTreeClassifier #https:/https://github.com/microsoft/pyright/blob/main/docs/configuration.md#reportMissingModuleSource/scikit-learn.org/stable/modules/generated/sklearn.tree.DecisionTreeClassifier.html#sklearn.tree.DecisionTreeClassifier
+from sklearn.tree import DecisionTreeClassifier #https://scikit-learn.org/stable/modules/generated/sklearn.tree.DecisionTreeClassifier.html
 from sklearn.neural_network import MLPClassifier #https://scikit-learn.org/stable/modules/generated/sklearn.neural_network.MLPClassifier.html#sklearn.neural_network.MLPClassifier
 from sklearn.model_selection import GridSearchCV #https://scikit-learn.org/stable/modules/generated/sklearn.model_selection.GridSearchCV.html
 from sklearn.metrics import classification_report, accuracy_score, f1_score, confusion_matrix
+from sklearn.tree import plot_tree #https://scikit-learn.org/stable/modules/generated/sklearn.tree.plot_tree.html#sklearn.tree.plot_tree   This will be used in 4.B
 
 
 def string_to_int(str) -> int:
@@ -42,7 +43,7 @@ def plot_distribution(type: pd, class_column: str, file_name: str):
     plt.title(f'{class_column} Class Distribution')
     
     # Save the plot as a PNG file
-    plt.savefig(file_name)
+    #plt.savefig(file_name)
     plt.show()
 
 def read_csv(file_path):
@@ -78,6 +79,39 @@ def penguinsSteps(penguin_data):
     print('Base-DT Performance for Penguins:')
     #Comparing true results with prediction
     print(classification_report(y_test_penguins, y_pred_baseDT, zero_division=1))
+
+    #4b): Top-DT for Penguins
+    # Define hyperparameters to search for
+    param_grid = {
+        'criterion': ['gini', 'entropy'], #Using gini or entorpy as mention in the Insturctions for the decisionTree
+        'max_depth': [None, 10, 100],  # 2 different values of our choose including a ”None” option. Values were chosen random.
+        'min_samples_split': [2, 5, 20],  # 3 different values of our choice. Values were chosen random.
+    }
+
+    # Create a Decision Tree classifier like in 4a from Scikit-Learn
+    topDT = DecisionTreeClassifier()
+
+    # Finding the hyperparameters using a GridSearchCv function from Scikit-Learns.
+    grid_search = GridSearchCV(topDT, param_grid, cv=5, scoring='accuracy')
+    grid_search.fit(X_train_penguins, y_train_penguins)
+
+    # Getting the best hyperparameters based on our grid_search.
+    best_params = grid_search.best_params_ #the .best_params_ gives the best results on the hold out data
+    print('Best Hyperparameters:', best_params)
+
+    # Train the Top-DT with the best hyperparameters
+    topDT = DecisionTreeClassifier(**best_params) #The two stars **unpacks the variable, equivalent to placing it as DecisionTreeClassifier(criterion='entropy', max_depth=10, min_samples_split=5). Though, we don't know the best parameters. 
+    topDT.fit(X_train_penguins, y_train_penguins)
+    y_pred_topDT = topDT.predict(X_test_penguins)
+
+    print('Top-DT Performance for Penguins:')
+    print(classification_report(y_test_penguins, y_pred_topDT, zero_division=1))
+
+    #Showing the decision tree graphically (depth is restricted for visualization purposes)
+    plt.figure(figsize=(10, 8)) #10 inches by 8 inches. Reduce the figure size if the monitor/screen is small.
+    plot_tree(topDT, filled=True, feature_names=X_penguins.columns, class_names=y_penguins.unique())
+    plt.show()
+    #plot_distribution(penguins, "species", "penguins-classes-topDT.png")
 
 def main():
     script_directory = os.path.dirname(os.path.abspath(__file__))  # Get the directory/filepath of the script
